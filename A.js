79aeb5ee5174a2154b5f9b718805a3651afb9c8f
dangtuandat123 <79784509+dangtuandat123@users.noreply.github.com>
2023-09-11 07:19:30 +0700

    Update A.js

--- A.js
+++ A.js
@@ -1,1 +1,10 @@
-document.getElementsByClassName("glSvWf").innerHTML = "New text!";
+ function copyText() {
+            var textToCopy = document.querySelector('.copyable').textContent;
+            var tempInput = document.createElement("input");
+            tempInput.value = textToCopy;
+            document.body.appendChild(tempInput);
+            tempInput.select();
+            document.execCommand("copy");
+            document.body.removeChild(tempInput);
+            alert("Đã sao chép: " + textToCopy);
+        }
